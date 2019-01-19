5ffcb42a87528867ca7a86fdaa6e1e0f5822987e
Alan M. Carroll <amc@apache.org>
2019-01-19 17:39:11 -0600

    Initial project setup, still needs tuning.

---
+++ txn_box/src/txn_box.cc
@@ -0,0 +1,44 @@
+/* 
+   Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements.
+   See the NOTICE file distributed with this work for additional information regarding copyright
+   ownership.  The ASF licenses this file to you under the Apache License, Version 2.0 (the
+   "License"); you may not use this file except in compliance with the License.  You may obtain a
+   copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software distributed under the License
+   is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+   or implied. See the License for the specific language governing permissions and limitations under
+   the License.
+   
+*/
+
+#include "ts/ts.h"
+
+#include "swoc/TextView.h"
+
+#define PLUGIN_NAME "Transaction Tool Box"
+#define PLUGIN_TAG "txn_box"
+
+using ts::TextView;
+
+/* ------------------------------------------------------------------------------------ */
+
+void
+TSPluginInit(int argc, char const *argv[])
+{
+  TSPluginRegistrationInfo info{PLUGIN_NAME, "Oath:", "solidwallofcode@oath.com"};
+
+  if (TSPluginRegister(&info) != TS_SUCCESS) {
+    TSError(PLUGIN_NAME ": plugin registration failed.");
+  }
+
+  Config.load_config(argc - 1, argv + 1);
+  if (Config.count() <= 0) {
+    TSError("%s: No destinations defined, plugin disabled", PLUGIN_TAG);
+  }
+
+  TSCont contp = TSContCreate(CB_Read_Request_Hdr, TSMutexCreate());
+  TSHttpHookAdd(TS_HTTP_READ_REQUEST_HDR_HOOK, contp);
+}
