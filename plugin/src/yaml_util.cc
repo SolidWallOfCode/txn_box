bdac177125f6efcb84da25f089171656a6ecf9d8
Alan M. Carroll <amc@tidus.solidwallofcode.com>
2020-01-17 18:10:14 -0600

    Fix: use ATS YAML CPP, don't build it.

--- plugin/src/yaml_util.cc
+++ plugin/src/yaml_util.cc
@@ -16,7 +16,46 @@ using namespace swoc::literals;
 using swoc::Errata;
 using swoc::Rv;
 
+/* ------------------------------------------------------------------------------------ */
+YAML::Node yaml_merge(YAML::Node root) {
+  static constexpr auto flatten = [] (YAML::Node dst, YAML::Node const& src) -> void {
+    if (src.IsMap()) {
+      for ( auto const& [ key, value ] : src ) {
+        // don't need to check for nested merge key, because this function is called only if
+        // that's already set in @a dst therefore it won't be copied up from @a src.
+        if (!dst[key]) {
+          dst[key] = value;
+        }
+      }
+    }
+  };
+
+  if (root.IsSequence()) {
+    for ( auto child : root ) {
+      yaml_merge(child);
+    }
+  } else if (root.IsMap()) {
+    // Do all nested merges first, so the result is iteration order independent.
+    for ( auto [ key, value ] : root ) {
+      value = yaml_merge(value);
+    }
+    // If there's a merge key, merge it in.
+    if ( auto merge_node { root[YAML_MERGE_KEY] } ; merge_node ) {
+      if (merge_node.IsMap()) {
+        flatten(root, merge_node);
+      } else if (merge_node.IsSequence()) {
+        for (auto src : merge_node) {
+          flatten(root, src);
+        }
+      }
+      root.remove(YAML_MERGE_KEY);
+    }
+  }
+  return root;
+}
+
 Rv<YAML::Node> yaml_load(swoc::file::path const& path) {
+//  static_assert(sizeof(YAML::Node) == sizeof(static_cast<Rv<YAML::Node>*>(nullptr)->_r));
   std::error_code ec;
   std::string content = swoc::file::load(path, ec);
 
@@ -32,5 +71,8 @@ Rv<YAML::Node> yaml_load(swoc::file::path const& path) {
   }
 
   yaml_merge(root);
+  [[maybe_unused]] auto s1 = sizeof(Rv<YAML::Node>::result_type);
+  [[maybe_unused]] auto s2 = sizeof(YAML::Node);
+  static_assert(sizeof(Rv<YAML::Node>) > sizeof(YAML::Node));
   return root;
 }
