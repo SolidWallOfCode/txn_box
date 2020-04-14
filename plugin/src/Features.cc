26633241d60ff55c9adc455ee8d1435f69b2ca74
Alan M. Carroll <amc@apache.org>
2020-04-14 09:11:18 -0500

    Change duration extractor to template.

--- plugin/src/Features.cc
+++ plugin/src/Features.cc
@@ -734,12 +734,12 @@ Rv<ValueType> Ex_random::validate(Config &cfg, Extractor::Spec &spec, TextView c
   return INTEGER;
 }
 /* ------------------------------------------------------------------------------------ */
-class Ex_nanoseconds : public Extractor {
-  using self_type = Ex_nanoseconds; ///< Self reference type.
+template < typename T, const TextView* KEY> class Ex_duration : public Extractor {
+  using self_type = Ex_duration; ///< Self reference type.
   using super_type = Extractor; ///< Parent type.
   using ftype = feature_type_for<DURATION>;
 public:
-  static constexpr TextView NAME { "nanoseconds" };
+  static constexpr TextView NAME { *KEY };
 
   Rv<ValueType> validate(Config & cfg, Spec & spec, TextView const& arg) override;
 
@@ -749,15 +749,15 @@ public:
   BufferWriter& format(BufferWriter& w, Spec const& spec, Context& ctx) override;
 };
 
-Feature Ex_nanoseconds::extract(Context &ctx, Extractor::Spec const& spec) {
+template < typename T, const TextView* KEY > Feature Ex_duration<T,KEY>::extract(Context &ctx, Extractor::Spec const& spec) {
   return spec._data.rebind<ftype>()[0];
 };
 
-BufferWriter& Ex_nanoseconds::format(BufferWriter &w, Extractor::Spec const &spec, Context &ctx) {
+template < typename T, const TextView* KEY > BufferWriter& Ex_duration<T,KEY>::format(BufferWriter &w, Extractor::Spec const &spec, Context &ctx) {
   return bwformat(w, spec, this->extract(ctx, spec));
 }
 
-Rv<ValueType> Ex_nanoseconds::validate(Config &cfg, Extractor::Spec &spec, TextView const &arg) {
+template < typename T, const TextView* KEY> Rv<ValueType> Ex_duration<T,KEY>::validate(Config &cfg, Extractor::Spec &spec, TextView const &arg) {
   auto span = cfg.span<ftype>(1);
   spec._data = span.rebind<void>(); // remember where the storage is.
 
@@ -771,8 +771,7 @@ Rv<ValueType> Ex_nanoseconds::validate(Config &cfg, Extractor::Spec &spec, TextV
     return Error(R"(Parameter "{}" for "{}" is not an integer as required)", arg, NAME);
   }
 
-  // Update the stored values now that *both* input values are validated.
-  span[0] = ftype{n};
+  span[0] = T{n};
   return DURATION;
 }
 /* ------------------------------------------------------------------------------------ */
@@ -857,6 +856,11 @@ Ex_cssn_remote_addr cssn_remote_addr;
 
 Ex_random random;
 
+static constexpr TextView NANOSECONDS = "nanoseconds";
+Ex_duration<std::chrono::nanoseconds, &NANOSECONDS> nanoseconds;
+static constexpr TextView SECONDS = "seconds";
+Ex_duration<std::chrono::seconds, &SECONDS> seconds;
+
 Ex_active_feature ex_with_feature;
 Ex_remainder_feature ex_remainder_feature;
 
@@ -890,6 +894,9 @@ Ex_remainder_feature ex_remainder_feature;
   Extractor::define(Ex_cssn_sni::NAME, &cssn_sni);
   Extractor::define(Ex_cssn_proto::NAME, &cssn_proto);
   Extractor::define(Ex_cssn_remote_addr::NAME, &cssn_remote_addr);
+
+  Extractor::define(NANOSECONDS, &nanoseconds);
+  Extractor::define(SECONDS, &seconds);
 
   return true;
 } ();
