909fc19475687bd446486775e150cb9f847c71c5
bneradt <bneradt@verizonmedia.com>
2020-05-19 20:40:08 +0000

    AuTest remap_config updates.

--- test/autest/gold_tests/static_file/static_file.test.py
+++ test/autest/gold_tests/static_file/static_file.test.py
@@ -10,9 +10,9 @@ Static file serving and handling.
 Test.Summary = '''
 Server static file as response body.
 '''
-r = Test.TxnBoxTestRun("Test static file support", "static_file.replay.yaml", config_key="meta.txn_box")
+r = Test.TxnBoxTestRun("Test static file support", "static_file.replay.yaml", config_key="meta.txn_box",
+                       remap_configs=[('http://example.one', 'http://example.one',
+                                      ['static_file.replay.yaml', 'meta.txn-box-remap'])])
 ts = r.Variables.TS
+server = r.Variables.SERVER
 ts.Setup.Copy("static_file.txt", ts.Variables.CONFIGDIR)
-ts.Disk.remap_config.AddLine(
-    'map http://example.one http://example.one @plugin=txn_box.so @pparam=static_file.replay.yaml @pparam=meta.txn-box-remap'
-)
