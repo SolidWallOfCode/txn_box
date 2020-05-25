657b3e7f78bfe4c30cc24b4bd86c624b5932ec34
Alan M. Carroll <amc@apache.org>
2020-05-25 16:26:11 -0500

    Fix static_file test to conform to new plugin loading spec.

--- test/autest/gold_tests/static_file/static_file.test.py
+++ test/autest/gold_tests/static_file/static_file.test.py
@@ -12,7 +12,7 @@ Server static file as response body.
 '''
 r = Test.TxnBoxTestRun("Test static file support", "static_file.replay.yaml", config_key="meta.txn_box",
                        remap=[('http://example.one', 'http://example.one',
-                             ['static_file.replay.yaml', 'meta.txn-box-remap'])])
+                             [ '--key=meta.txn-box-remap', 'static_file.replay.yaml' ])])
 ts = r.Variables.TS
 server = r.Variables.SERVER
 ts.Setup.Copy("static_file.txt", ts.Variables.CONFIGDIR)
