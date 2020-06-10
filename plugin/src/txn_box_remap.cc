51334c175e5fe908f087caafa30b977151fe5327
Alan M. Carroll <amc@apache.org>
2020-06-10 15:29:09 -0500

    Fix remap reload failure. * Change of signature for remap reload signal * Disable remap cache for ATS 7 since it can't be cleared reliably.

--- plugin/src/txn_box_remap.cc
+++ plugin/src/txn_box_remap.cc
@@ -33,104 +33,7 @@ namespace bwf = swoc::bwf;
 using namespace swoc::literals;
 
 /* ------------------------------------------------------------------------------------ */
-# if 0
-/** Data shared among all instances in a remap configuration.
- *
- * This class has some odd behaviors due to the way the remap plugin API works.
- *
- * A global instance is available while the rule instances are being loaded, but after all of them
- * have been loaded, the global instance is cleared so that a new instance is created on the next
- * configuration load.
- *
- * Each rule instance gets a pointer to the global instance for its configuration. The instances
- * reference count that instance and when the last rule is deleted, the instance is cleaned up. In
- * effect, each rule instance acts as a smart pointer. This can't use a normal smart pointer because
- * only a raw pointer can be stored in the rule instance context.
- */
-class ConfigFileCache {
-  using self_type = ConfigFileCache; ///< Self reference type.
-public:
-  /// Configurations from a single file.
-  struct ConfigFile {
-    YAML::Node _root; ///< Root of YAMl in file.
-    /// Configurations from file, indexed by base key of the configuration.
-    std::unordered_map<std::string, Config> _cfgs;
-  };
-
-  /// Obtain the active instance.
-  static self_type & acquire();
-
-  /// Release @a this for potential cleanup.
-  void release();
-
-  /// Drop active instance, force new instance on next @c acquire.
-  static void clear();
-
-  /** Get the @c ConfigFile for the file @a path.
-   *
-   * @param path Absolute path to configuration file.
-   * @return The @c ConfigFile or errors if the file cannot be loaded and parsed.
-   *
-   * If the file is not already in the table, it is loaded and parsed and the table updated.
-   */
-  Rv<ConfigFile *> obtain(swoc::file::path const& path);
-
-protected:
-  using ConfigTable = std::unordered_map<std::string, ConfigFile>;
-  ConfigTable _cfg_table;
-
-  unsigned _ref_count { 0 }; ///< Reference count.
-
-  /// Active instance.
-  static self_type * _instance;
-
-  ConfigFileCache() = default;
-  ConfigFileCache(self_type const& that) = delete;
-  self_type & operator = (self_type const& that) = delete;
-};
-
-ConfigFileCache* ConfigFileCache::_instance = nullptr;
-
-ConfigFileCache::self_type &ConfigFileCache::acquire() {
-  #if TS_VERSION_MAJOR < 8
-  // pre-8, there's no general reload signal, so must just have a separate instance for each
-  // rule.
-  auto self = new self_type;
-  ++(self->_ref_count);
-  return *self;
-  #else
-  if (nullptr == _instance) {
-    _instance = new self_type;
-  }
-  ++(_instance->_ref_count);
-  return *_instance;
-  #endif
-}
-
-void ConfigFileCache::release() {
-  if (--_ref_count == 0) {
-    delete this;
-  }
-}
-
-void ConfigFileCache::clear() { _instance = nullptr; }
-
-Rv<ConfigFileCache::ConfigFile *> ConfigFileCache::obtain(swoc::file::path const &path) {
-  if ( auto spot = _cfg_table.find(path.string()) ; spot != _cfg_table.end()) {
-    return &spot->second;
-  }
-  // Try loading and parsing the file.
-  auto && [ root, errata ] { yaml_load(path) };
-  if (! errata.is_ok()) {
-    errata.info(R"(While loading file "{}".)", path);
-    return std::move(errata);
-  }
-  auto & cg = _cfg_table[path.string()];
-  cg._root = root; // looks good, stash it.
-  return &cg;
-}
-#endif
-Config::YamlCache Yaml_Cache;
+Config::YamlCache Remap_Cfg_Cache;
 /* ------------------------------------------------------------------------------------ */
 class RemapContext {
   using self_type = RemapContext; ///< Self reference type.
@@ -152,9 +55,11 @@ TSRemapInit(TSRemapInterface*, char* errbuff, int errbuff_size) {
   return TS_SUCCESS;
 };
 
-void TSRemapConfigReload() {
-  Yaml_Cache.clear();
+#if TS_VERSION_MAJOR >= 8
+void TSRemapPostConfigReload(TSRemapReloadStatus) {
+  Remap_Cfg_Cache.clear();
 }
+#endif
 
 TSReturnCode TSRemapNewInstance(int argc, char *argv[], void ** ih, char * errbuff, int errbuff_size) {
   swoc::FixedBufferWriter w(errbuff, errbuff_size);
@@ -167,7 +72,12 @@ TSReturnCode TSRemapNewInstance(int argc, char *argv[], void ** ih, char * errbu
   std::unique_ptr<Config> cfg { new Config };
   swoc::MemSpan<char const *> span{ swoc::MemSpan<char*>(argv, argc).rebind<char const *>() };
   cfg->mark_as_remap();
-  Errata errata = cfg->load_args(span, 2, &Yaml_Cache);
+  Errata errata = cfg->load_args(span, 2
+#if TS_VERSION_MAJOR >= 8
+      // pre ATS 8 doesn't support remap reload callbacks, so the cache can't be used.
+      , &Remap_Cfg_Cache
+#endif
+  );
 
   if (!errata.is_ok()) {
     std::string text;
