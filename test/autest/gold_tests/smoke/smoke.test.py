83eaff60d1c53831acc071e39569ca169e358fb6
Alan M. Carroll <amc@apache.org>
2020-07-06 09:54:20 -0500

    Remove dead code in smoke test.

--- test/autest/gold_tests/smoke/smoke.test.py
+++ test/autest/gold_tests/smoke/smoke.test.py
@@ -10,11 +10,6 @@ Test.Summary = '''
 Test basic functions and directives.
 '''
 
-#Test.TxnBoxTest("smoke.replay.yaml", config_key="meta.txn_box"
-#                ,remap=[('http://example.one', 'http://example.one')]
-#                )
-#Test.TxnBoxRun("Smoke test")
-
 Test.TxnBoxTestAndRun("Smoke Test", "smoke.replay.yaml", config_path='Auto', config_key="meta.txn_box.global"
                 ,remap=[('http://example.one/3', 'http://example.one/3', ('--key=meta.txn_box.remap-1', 'smoke.replay.yaml'))
                        ,('http://example.one', 'http://example.one')
