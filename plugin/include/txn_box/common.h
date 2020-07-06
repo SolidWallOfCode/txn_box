d5cba9353fc51db7aa977ebd02233e9e3e8c1ae8
Alan M. Carroll <amc@apache.org>
2020-07-06 09:52:33 -0500

    Add BWF override for monostate to avoid ambiguity.

--- plugin/include/txn_box/common.h
+++ plugin/include/txn_box/common.h
@@ -653,6 +653,7 @@ template < typename T > let<T>::~let() { _var = _value; }
 
 // BufferWriter support.
 namespace swoc {
+BufferWriter &bwformat(BufferWriter& w, bwf::Spec const& spec, std::monostate);
 BufferWriter &bwformat(BufferWriter &w, bwf::Spec const &spec, ValueType type);
 BufferWriter &bwformat(BufferWriter &w, bwf::Spec const &spec, Feature const &feature);
 BufferWriter &bwformat(BufferWriter &w, bwf::Spec const &spec, ValueMask const &mask);
