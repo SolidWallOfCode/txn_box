dd61f84dd32aae7f09d8ae786bd28c343b5b3b91
Alan M. Carroll <amc@apache.org>
2020-08-13 08:50:49 -0500

    Enable debug directive on post-load hook.

--- test/autest/gold_tests/static_file/static_file.test.py
+++ test/autest/gold_tests/static_file/static_file.test.py
@@ -17,3 +17,7 @@ r = Test.TxnBoxTestAndRun("Test static file support", "static_file.replay.yaml"
                               [ '--key=meta.txn-box-remap', 'static_file.replay.yaml' ]]])
 ts = r.Variables.TS
 ts.Setup.Copy("static_file.txt", ts.Variables.CONFIGDIR)
+ts.Disk.records_config.update({
+    'proxy.config.diags.debug.enabled': 1
+    , 'proxy.config.diags.debug.tags' : 'txn_box'
+})
