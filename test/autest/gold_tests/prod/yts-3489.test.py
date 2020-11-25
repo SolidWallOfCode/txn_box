28495545ade98dd1707742aa8dda98a8895e8821
Alan M. Carroll <amc@apache.org>
2020-11-25 14:24:52 -0600

    Add explicit redirect test, keep simplified version as a production use case example.

---
+++ test/autest/gold_tests/prod/yts-3489.test.py
@@ -0,0 +1,29 @@
+# @file
+#
+# Copyright 2020, Verizon Media
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import os.path
+
+Test.Summary = '''
+Production use case: Use redirect to clip the path for specific URL paths.
+'''
+
+replay_file="yts-3489.replay.yaml"
+
+tr = Test.TxnBoxTestAndRun("Redirect", replay_file
+                , remap=[ ['http://base.ex/',  ( '--key=meta.txn_box.remap', replay_file) ]
+                        ]
+                , verifier_client_args="--verbose info"
+                )
+
+ts = tr.Variables.TS
+
+ts.Setup.Copy(replay_file, ts.Variables.CONFIGDIR) # because it's remap only - not auto-copied.
+
+ts.Disk.records_config.update({
+      'proxy.config.diags.debug.enabled': 1
+    , 'proxy.config.diags.debug.tags': 'txn_box'
+    , 'proxy.config.http.cache.http':  0
+})
