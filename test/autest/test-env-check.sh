945e2927bcff6fd686d88ac2f495236c70941b69
bneradt <bneradt@verizonmedia.com>
2021-02-02 21:59:54 +0000

    Updating Pipenv and test-env-check.sh to latest ATS versions

--- test/autest/test-env-check.sh
+++ test/autest/test-env-check.sh
@@ -20,38 +20,34 @@
 python3 - << _END_
 import sys
 
-if sys.version_info.major < 3 or sys.version_info.minor < 5:
+if sys.version_info.major < 3 or sys.version_info.minor < 6:
     exit(1)
 _END_
 
-if [ $? = 1 ]
-then
-    echo "Python 3.5 or newer is not installed/enabled."
-    exit 1
+if [ $? = 1 ]; then
+    echo "Python 3.6 or newer is not installed/enabled."
+    return
 else
-    echo "Python 3.5 or newer detected!"
+    echo "Python 3.6 or newer detected!"
 fi
 
 # check for python development header -- for autest
 python3-config &> /dev/null
-if [ $? = 1 ]
-then
+if [ $? = 1 ]; then
     echo "python3-dev/devel detected!"
 else
     echo "python3-dev/devel is not installed. "
-    exit 1
+    return
 fi
 
 # check for pipenv
 pipenv --version &> /dev/null
-if [ $? -eq 0 ]
-then
+if [ $? -eq 0 ]; then
     echo "pipenv detected!"
     pipenv --venv &> /dev/null
-    if [ $? -ne 0 ]
-    then
+    if [ $? -ne 0 ]; then
         echo "Installing a new virtual environment via pipenv"
-    pipenv install
+        pipenv install
     else
         echo "Using the pre-existing virtual environment."
     fi
