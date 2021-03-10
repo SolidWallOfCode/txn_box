7ff865af05b8e00cb90257005ecbb0c298472ba9
Alan M. Carroll <amc@apache.org>
2021-03-10 16:03:32 -0600

    Change TSNote to TSError for pre-ATS 10 versions.

--- plugin/src/text_block.cc
+++ plugin/src/text_block.cc
@@ -282,7 +282,12 @@ void Do_text_block_define::Updater::operator()() {
         auto msg = ctx.render_transient([&](BufferWriter& w){
           w.write(Config::PLUGIN_TAG).write(": ").write(text);
         });
+        // Note - convert this to templates.
+        #if TS_VERSION_MAJOR < 10
+        TSError("%.*s", int(msg.size()), msg.data());
+        #else
         TSNote("%.*s", int(msg.size()), msg.data());
+        #endif
       }
       return;
     }
