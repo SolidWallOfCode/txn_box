10901cbe83f858abb5e712231e9158b1041449bf
Alan M. Carroll <amc@apache.org>
2021-08-02 11:30:50 -0500

    Update to version 0.4.0

--- doc/conf.py
+++ doc/conf.py
@@ -78,9 +78,9 @@ copyright = u'2020, Verizon Media'
 # update a reasonable version number here.
 #
 # The short X.Y version.
-version = "0.3"
+version = "0.4"
 # The full version, including alpha/beta/rc tags.
-release = "0.3.20"
+release = "0.4.0"
 
 extlinks = { 'git': ( 'https://github.com/solidwallofcode/txn_box/tree/{}/%s'.format(release) , '') }
 
