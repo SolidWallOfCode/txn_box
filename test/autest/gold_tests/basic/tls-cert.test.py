5ff35dfba55ef26a63e65a2a711c3b8f4e8fb752
Alan M. Carroll <amc@apache.org>
2022-08-31 09:15:06 -0500

    Test: Update cert testing for not sending client cert outbound.

--- test/autest/gold_tests/basic/tls-cert.test.py
+++ test/autest/gold_tests/basic/tls-cert.test.py
@@ -13,14 +13,17 @@ Test certificate handling.
 tr = Test.TxnBoxTestAndRun("TLS Certs", "tls-cert.replay.yaml"
                           , config_path='Auto', config_key="meta.txn_box.global"
                           , enable_tls=True
-                          , remap=[ ['https://alpha.ex/' , "https://alpha.ex/"] ]
+                          , remap=[ ['https://alpha.ex/' , "https://alpha.ex/"]
+                                  , ['http://alpha.ex/'  , 'https://alpha.ex/']
+                                  , ['http://charlie.ex/'  , 'https://charlie.ex/']
+                                  ]
                           )
 
 ts = tr.Variables.TS
 
-ts.Setup.Copy("tls-cert.replay.yaml", ts.Variables.CONFIGDIR) # because it's remap only - not auto-copied.
 ts.Setup.Copy("../../ssl/server.key", os.path.join(ts.Variables.CONFIGDIR, "server.key"))
 ts.Setup.Copy("../../ssl/server.pem", os.path.join(ts.Variables.CONFIGDIR, "server.pem"))
+ts.Setup.Copy("../../ssl/bravo-signed.cert", os.path.join(ts.Variables.CONFIGDIR, "bravo-signed.cert"))
 
 ts.Disk.records_config.update({
       'proxy.config.diags.debug.enabled': 1
@@ -33,6 +36,8 @@ ts.Disk.records_config.update({
     , 'proxy.config.http.server_ports': '{0} {1}:ssl'.format(ts.Variables.port, ts.Variables.ssl_port)
     , 'proxy.config.ssl.client.certification_level': 0
     , 'proxy.config.ssl.client.verify.server.policy': 'DISABLED'
+    , 'proxy.config.ssl.client.cert.path': ts.Variables.CONFIGDIR
+    , 'proxy.config.ssl.client.cert.filename': "bravo-signed.cert"
 })
 ts.Disk.ssl_multicert_config.AddLine(
     'dest_ip=* ssl_cert_name=server.pem ssl_key_name=server.key'
